c1f365446442cbf2d802b622d29ec587887b48fa
Rezlind <harryreardon@hotmail.co.uk>
2022-03-24 18:31:08 +0000

    Update CfgRemoteExec.hpp

--- Altis_Life.Altis/CfgRemoteExec.hpp
+++ Altis_Life.Altis/CfgRemoteExec.hpp
@@ -15,6 +15,7 @@ class CfgRemoteExec {
     class Functions {
         mode = 1;
         jip = 0;
+        #include "rezlind\rezlind_remoteExec.cpp"
 
         /* Client only functions */
         F(life_fnc_AAN,CLIENT)
